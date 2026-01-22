1d3969a690f0a1e6e90967b0b265d5619fa06be0
louis <corneliuslouis3112@gmail.com>
2026-01-22 11:32:52 +0700

    feat(authController): implement register, login, access me, and logout endpoints for users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * POST /api/auth/register
+    */
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:student,tutor',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => $request->role,
+        ]);
+
+        $token = auth()->guard('api')->login($user);
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ], 201);
+    }
+
+    /**
+     * POST /api/auth/login
+    */
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->guard('api')->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status' => 'error',    
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * GET /api/auth/me
+    */
+    public function me(){
+        return response()->json(auth()->guard('api')->user());
+    }
+
+    /**
+     * POST /api/auth/logout
+    */
+    public function logout(){
+        auth()->guard('api')->logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Response token
+    */
+    protected function respondWithToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->guard('api')->factory()->getTTL() * 60,
+            'user' => auth()->guard('api')->user()
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/me', [AuthController::class, 'me']);
+});
